8ef1fdf9e6d952b978a1b7768551c4d70b64d84f
Colin Perkins <csp@csperkins.org>
2019-08-30 12:55:16 +0100

    Fix license metadata

--- setup.py
+++ setup.py
@@ -5,17 +5,17 @@ with open("README.md", "r") as fh:
 
 setuptools.setup(
     name="ietfdata",
-    version="0.1.0",
+    version="0.1.1",
     author="Colin Perkins",
     author_email="csp@csperkins.org",
-    description="Access the IETF DataTracker and RFC Index",
+    description="Access the IETF Data Tracker and RFC Index",
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/csperkins/ietfdata",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
-        "License :: OSI Approved :: MIT License",
+        "License :: OSI Approved :: BSD License",
         "Operating System :: OS Independent",
     ],
     python_requires='>=3.7',
