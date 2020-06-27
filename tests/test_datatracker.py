2741372125477b6b6bed56863bf916d45e695785
Colin Perkins <csp@csperkins.org>
2020-06-27 19:59:03 +0100

    Update tests

--- tests/test_datatracker.py
+++ tests/test_datatracker.py
@@ -1575,7 +1575,7 @@ class TestDatatracker(unittest.TestCase):
 
     def test_group_role_histories_email(self) -> None:
         group_role_histories = list(self.dt.group_role_histories(email="csp@csperkins.org"))
-        self.assertEqual(len(group_role_histories), 27)
+        self.assertEqual(len(group_role_histories), 28)
 
 
     def test_group_role_histories_group(self) -> None:
@@ -1594,7 +1594,7 @@ class TestDatatracker(unittest.TestCase):
 
     def test_group_role_histories_person(self) -> None:
         group_role_histories = list(self.dt.group_role_histories(person=self.dt.person(PersonURI("/api/v1/person/person/20209/"))))
-        self.assertEqual(len(group_role_histories), 27)
+        self.assertEqual(len(group_role_histories), 28)
 
 
     def test_group_state_change_event(self) -> None:
