f3553aa41f7425f3e92f9c1d18cbb4125ae5d964
Stephen McQuistin <sm@smcquistin.uk>
2020-07-06 12:34:14 +0100

    An Email may be in the Datatracker and not associated with a Person (e.g., https://datatracker.ietf.org/api/v1/person/email/execd@iab.org/)

--- ietfdata/datatracker.py
+++ ietfdata/datatracker.py
@@ -1883,7 +1883,7 @@ class DataTracker:
 
     def person_from_email(self, email_addr: str) -> Optional[Person]:
         email = self.email(EmailURI("/api/v1/person/email/" + email_addr + "/"))
-        if email is not None:
+        if email is not None and email.person is not None:
             return self.person(email.person)
         else:
             return None
