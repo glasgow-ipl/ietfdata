0feed1d31256f2a67cbb2c2e698191b6dbe80524
Ryo Yanagida <Ryo.Yanagida@glasgow.ac.uk>
2025-02-21 11:36:28 +0000

    Sketching affiliations.py

--- ietfdata/tools/affiliations.py
+++ ietfdata/tools/affiliations.py
@@ -14,5 +14,26 @@ import mailarchive_mbox
 # 3. [PID] -> [years] -> [affiliations] mappings from documents 
 # 4. [PID] -> [years] -> [affiliations] mappings from emails
 
-# normalisation strings
 
+if __name-_ == "__main__":
+    # normalisation patterns
+    # Load normalisation patterns
+    
+    
+    # Load Raw affil. -> normalised affil. mapping
+    
+    
+    # Load email_domain -> normalised affil. mapping
+    
+    # Load public suffix list https://publicsuffix.org/list/ (mozilla)
+    
+    # Load Participants file
+    
+    
+    #========================================================================#
+    # Go through RFC Documents
+    
+    
+    
+    #========================================================================#
+    # Go through Emails
