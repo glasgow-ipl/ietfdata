e28fa5cc899168297513472c752148f31ddf1e83
Ryo Yanagida <Ryo.Yanagida@glasgow.ac.uk>
2025-02-21 18:04:02 +0000

    attempting to construct

--- ietfdata/tools/affiliations.py
+++ ietfdata/tools/affiliations.py
@@ -2,6 +2,9 @@ import sys
 import csv
 import json 
 
+from datetime    import timedelta
+
+
 from ietfdata.datatracker     import *
 from ietfdata.datatracker_ext import *
 from ietfdata.mailarchive2    import *
@@ -11,15 +14,76 @@ import mailarchive_mbox
 # Mappings generated:
 # 1. raw affiliation -> normalised affiliation mappings
 # 2. email_domain -> normalised affiliations mappings
-# 3. [PID] -> [years] -> [affiliations] mappings from documents 
-# 4. [PID] -> [years] -> [affiliations] mappings from emails
+# 3. identity -> start year-month, end year-month, affiliation mapping
 
+# Affiliation Class
+class AffiliationEntry:
+    start_date  : date
+    end_date    : date
+    affiliation : str
+
+class Affiliations:
+    entries # entries of affiliations
+    
+    def __init__(self, input_file:Optional[Path]):
+        if input_file is None:
+            self.entries = dict()
+            return
+        if Path(os.path.isfile(input_file)):
+            with open(input_file):
+                self.entries=json.load(file)
+    
+    def add_affiliation(self, date:date, affiliation_str:str, identifier:str):
+        if identifier not in self.entries:
+            self.entries[identifier]=dict()
+        for start_date in self.entries[identifier]:
+
+                
+    
+        
+        
+    
+
+def remove_suffix(input_string, suffix):
+    if suffix and input_string.endswith(suffix):
+        return input_string[:-len(suffix)]
+    return input_string
+
+
+def cleanup_affiliation(affiliation_str:str):
+    affiliation_str = affiliation_str.strip()
+    affiliation_str = affiliation_str.replace("\n","")
+    affiliation_str = re.sub(' +', ' ', affiliation_str)
+    affiliation_suffixes = [", Inc.", "Inc", "LLC", "Ltd", "Limited", "Incorporated", "GmbH", "Inc.", "Systems", "Corporation", "Corp.", "Ltd.", "Technologies", "AG", "B.V."]
+    alt_university = ["Univ.","Universtaet","Universteit","Universitaet","Université"]
+    for alt in alt_university:
+        affiliation_str = affiliation_str.replace(alt, "University")
+    affiliation_str = affiliation_str.replace("TU","Technical University of")
+    affiliation_str = affiliation_str.replace("U. of", "University of")
+    for suffix in affiliation_suffixes:
+        affiliation_str = remove_suffix(affiliation_str, suffix)
+    affiliation_str = affiliation_str.replace(",","")
+    affiliation_str = affiliation_str.strip()
+    affiliation_str = re.sub(r"^\.|\.$", "", affiliation_str)
+    return affiliation_str
 
 if __name-_ == "__main__":
+    if len(sys.argv) == 2:
+        old_path = None
+        new_path = Path(sys.argv[1])
+    elif len(sys.argv) == 3:
+        old_path = Path(sys.argv[1])
+        new_path = Path(sys.argv[2])
+    else:
+        print("Usage: python3 -m ietfdata.tools.affiliations [[new.json]")
+        print("   or: python3 -m ietfdata.tools.affiliations [old.json] [new.json]")
+        sys.exit(1)
+
+    
+    
     # normalisation patterns
     # Load normalisation patterns
-    
-    
+        
     # Load Raw affil. -> normalised affil. mapping
     
     
