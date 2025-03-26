89a83b1ac2b3229846547b21261c0cf4727f46f4
Colin Perkins <csp@csperkins.org>
2025-03-26 15:10:11 +0000

    Add example

---
+++ examples/rfc-per-year-ietf.py
@@ -0,0 +1,65 @@
+# Copyright (C) 2020 University of Glasgow
+#
+# Redistribution and use in source and binary forms, with or without
+# modification, are permitted provided that the following conditions
+# are met:
+#
+# 1. Redistributions of source code must retain the above copyright notice,
+#    this list of conditions and the following disclaimer.
+#
+# 2. Redistributions in binary form must reproduce the above copyright
+#    notice, this list of conditions and the following disclaimer in the
+#    documentation and/or other materials provided with the distribution.
+#
+# THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+# AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+# IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE
+# ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE
+# LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR
+# CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF
+# SUBSTITUTE GOODS OR SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS
+# INTERRUPTION) HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN
+# CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+# ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
+# POSSIBILITY OF SUCH DAMAGE.
+
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+from pathlib              import Path
+from ietfdata.datatracker import *
+from ietfdata.rfcindex    import *
+
+# =============================================================================
+
+ri = RFCIndex()
+
+rfcs_per_year = {}
+authors_per_year = {}
+
+# Find the number of RFCs published on the IETF stream in each year,
+# along with the number of unique authors per year.
+
+for rfc in ri.rfcs():
+    if rfc.stream == "IETF":
+        if rfc.year not in rfcs_per_year:
+            rfcs_per_year[rfc.year] = 0
+        rfcs_per_year[rfc.year] += 1
+
+        if rfc.year not in authors_per_year:
+            authors_per_year[rfc.year] = []
+        for author in rfc.authors:
+            if author not in authors_per_year[rfc.year]:
+                authors_per_year[rfc.year].append(author)
+
+rfcs = 0
+year = 2000
+print("# year rfcs rfc_cumulative authors")
+while year < 2025:
+    rfcs += rfcs_per_year[year]
+    print(f"  {year} {rfcs_per_year[year]:4} {rfcs:14} {len(authors_per_year[year]):7}")
+    year += 1
+
+
