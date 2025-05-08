4dcc509b435f85208726cd9b57234d6f822dbf0c
Ryo Yanagida <Ryo.Yanagida@glasgow.ac.uk>
2025-05-08 11:00:41 +0100

    fixing AffiliationEntry class

--- ietfdata/tools/participants_affiliations.py
+++ ietfdata/tools/participants_affiliations.py
@@ -15,26 +15,45 @@ from ietfdata.mailarchive2    import *
 # Todo: Class to go through datatracker to extract the information, clean up names, match two orgs etc.
 # Affiliation Entry Class 
 class AffiliationEntry:
-	start_date  : datetime.date
-	end_date    : Optional[datetime.date]
-	OIDs : list[str]
+	_start_date  : datetime.date
+	_end_date    : datetime.date
+	_OIDs : list[str]
 	def __init__(self, start_date:datetime.date, end_date:Optional[datetime.date],OIDs:list[str]):
-		self.OIDs=OIDs
+		self.OIDs=copy.deepcopy(OIDs) # prevent unintended modification of list
 		self.start_date = start_date
 		if end_date is None:
 			self.end_date = start_date
 		else:
 			self.end_date = end_date
+
+	def get_start_date(self)->datetime.date:
+		return self._start_date
+
+	def get_end_date(self)->datetime.date:
+		return self._end_date
 	
+	def get_OIDs(self) -> list[str]:
+		return self._OIDs
+	
+	def set_start_date(self,new_start_date:datetime.date)->None:
+		if new_start_date > self._end_date:
+			raise RuntimeError(f"Cannot set the start date ({new_start_date}) to be past the current end date {self._end_date}.")
+		self._start_date = new_start_date
+		
 	def set_end_date(self, new_end_date:datetime.date):
+		if new_end_date < self._start_date:
+			raise RuntimeError(f"Cannot set the end date ({new_end_date}) to be earlier than the current start date {self._start_date}.")
 		self.end_date = new_end_date
+
+	def set_start_end_dates(self, new_start_date:datetime.date, new_end_date:datetime.date) -> None:
+		if new_start_date > new_end_date:
+			raise RuntimeError(f"Cannot set the end date ({new_end_date}) to be earlier than the new start date {new_start_date}.")
+		self._start_date = new_start_date
+		self._end_date = new_end_date
 	
-	def set_affiliation_OIDs(self, OIDs:list[str]):
-		self.OIDs=OIDs
-	
-	def match_OIDs(self, OIDs:list[str]):
+	def match_OIDs(self, OIDs:list[str])->bool:
 		for OID in OIDs:
-			if OID not in self.OIDs:
+			if OID not in self._OIDs:
 				return False
 		return True
 	
@@ -49,25 +68,30 @@ class AffiliationEntry:
 
 # Sets of Affiliation for Person class
 class AffiliationsForPerson:
-	identifiers : list[str]
-	affiliations: list[AffiliationEntry]
+	_PID : str
+	_affiliations: Optional[list[AffiliationEntry]]
 	
-	def __init__(self,identifiers:list[str],affiliations:list[AffiliationEntry]):
-		self.identifiers = copy.deepcopy(identifiers)
-		self.affiliations = copy.deepcopy(affiliations)
+	def __init__(self,PID:str,affiliations:Optional[list[AffiliationEntry]]):
+		self._PID = PID 
+		self._affiliations = None
+		if affiliations is not None:
+			self._affiliations = affiliations
 	
-	def add_identifier(self, identifier:str):
-		self.identifiers.append(identifier)
+	def get_PID(self) -> str:
+		return self._PID
+		
+	def get_affiliations(self) -> list[AffiliationEntry]:
+		return self._affiliations
 	
 	def add_affiliation(self, aff_entry:AffiliationEntry):
 		# TODO: Go through the timeline, insert the entry 
 		new_date = aff_entry.start_date
-		for affil in self.affiliations:
-			i = self.affiliations.index(affil)
+		for affil in self._affiliations:
+			i = self._affiliations.index(affil)
 			if new_date <= affil.start_date:
-				self.affiliations.insert(i,aff_entry)
+				self._affiliations.insert(i,aff_entry)
 				return #inserted entry
-		self.affiliations.append(aff_entry)
+		self._affiliations.append(aff_entry)
 		return
 			
 	def consolidate(self):
@@ -75,7 +99,7 @@ class AffiliationsForPerson:
 		# This should only be run if and only if everything has been scraped
 		tmp_head_affil = None # temporary first affil in the batch
 		consolidated_affil = list()
-		for affil in self.affiliations:
+		for affil in self._affiliations:
 			if tmp_head_affil is None:
 				tmp_head_affil = copy.deepcopy(affil)
 				continue
@@ -86,11 +110,11 @@ class AffiliationsForPerson:
 					tmp_head_affil = copy.deepcopy(affil)
 			if(tmp_head_affil not in consolidated_affil):
 				consolidated_affil.append(tmp_head_affil)
-		self.affiliations = copy.deepcopy(consolidated_affil)
+		self._affiliations = copy.deepcopy(consolidated_affil)
 	
 	# def __str__(self):
-	# 	returnstr = '{"identifiers":['
-	# 	for ident in self.identifiers:
+	# 	returnstr = '{"PID":['
+	# 	for ident in self.PID:
 	# 		returnstr += f'"{ident}",'
 	# 	returnstr = returnstr[:-1] # strip last comma
 	# 	returnstr += "],"
