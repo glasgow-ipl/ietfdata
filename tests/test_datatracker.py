302ae3d50fd1d7d900e61f938574e326b8c2620c
Colin Perkins <csp@csperkins.org>
2026-05-03 09:18:43 +0100

    Update to match datatracker

--- tests/test_datatracker.py
+++ tests/test_datatracker.py
@@ -1955,7 +1955,7 @@ class TestDatatracker(unittest.TestCase):
 
     def test_group_role_histories_email(self) -> None:
         group_role_histories = list(self.dt.group_role_histories(email="csp@csperkins.org"))
-        self.assertEqual(len(group_role_histories), 132)
+        self.assertEqual(len(group_role_histories), 133)
 
 
     def test_group_role_histories_group(self) -> None:
@@ -1971,7 +1971,7 @@ class TestDatatracker(unittest.TestCase):
 
     def test_group_role_histories_person(self) -> None:
         group_role_histories = list(self.dt.group_role_histories(person=self.dt.person(PersonURI(uri="/api/v1/person/person/20209/"))))
-        self.assertEqual(len(group_role_histories), 132)
+        self.assertEqual(len(group_role_histories), 133)
 
 
     def test_group_state_change_event(self) -> None:
@@ -3721,7 +3721,7 @@ class TestDatatracker(unittest.TestCase):
 
 
     # -----------------------------------------------------------------------------------------------------------------------------
-    # Tests relating to statistics:
+    # Tests relating to meetings:
 
     def test_meeting_registration(self) -> None:
         reg = self.dt.meeting_registration(MeetingRegistrationURI(uri="/api/v1/stats/meetingregistration/42206/"))
