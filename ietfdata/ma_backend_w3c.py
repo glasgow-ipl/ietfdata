66f11abf049bf6f7726790d5cac4117f00b095d0
Colin Perkins <csp@csperkins.org>
2026-09-13 10:22:29 +0100

    Refactor

--- ietfdata/ma_backend_w3c.py
+++ ietfdata/ma_backend_w3c.py
@@ -98,6 +98,76 @@ class MailArchiveBackendW3C(MailArchiveBackend):
             print(f"ERROR: {resp.status_code} {index_url}")
 
 
+    def _fetch_message(self, msg_url, base_url):
+        time.sleep(self.delay)
+        resp = self.session.get(msg_url)
+        if resp.status_code == 200:
+            soup = BeautifulSoup(resp.text, 'html.parser')
+            main = soup.find("main", class_="mail")
+
+            headers = main.find("ul", class_="headers")
+            hdr_subject = "Subject: " + soup.find("h1").text.strip()
+            hdr_date    = headers.find("span", class_ = "date")
+            hdr_from    = headers.find("span", class_ = "from")
+            hdr_to      = headers.find("span", class_ = "to")
+            hdr_cc      = headers.find("span", class_ = "cc")
+            hdr_msg_id  = headers.find("span", class_ = "message-id")
+
+            msg_body = main.find("pre", class_ = "body")
+
+            attach_list = []
+            attachments = main.find("section", class_ = "message-body-part attachment-links")
+            if attachments is not None:
+                for attach in attachments.find_all("li"):
+                    parts = attach.text.split()
+                    if parts[1] == "attachment:":
+                        if len(parts) == 3 and parts[2] == "stored":
+                            attach_type = parts[0]
+                            attach_disp = "inline"
+                        else:
+                            attach_type = parts[0]
+                            attach_disp = "attach"
+                    else:
+                        print(f"ERROR: can't parse attachment")
+                        print(attach)
+                        sys.exit()
+                    attach_path = attach.find("a")["href"]
+                    attach_url  = base_url + attach_path
+                    attachment = {"media_type"  : attach_type,
+                                  "url"         : attach_url,
+                                  "path"        : attach_path,
+                                  "disposition" : attach_disp}
+                    attach_list.append(attachment)
+
+
+            footer = soup.find("footer")
+            in_reply_to = None
+            for fitem in footer.find_all("li"):
+                fheader = fitem.find("span", class_="heading")
+                if fheader is not None:
+                    if fheader.text.strip() == "In reply to" or fheader.text.strip() == "Maybe in reply to":
+                        anchor = fitem.find("a")
+                        if anchor.text == "Message archived in another list or period":
+                            in_reply_to = anchor["href"]
+                        else:
+                            in_reply_to = base_url + anchor["href"]
+
+
+            item = {"msg_url"      : msg_url,
+                    "subject"      : hdr_subject,
+                    "date"         : hdr_date.text.strip()    if hdr_date   is not None else None,
+                    "from"         : hdr_from.text.strip()    if hdr_from   is not None else None,
+                    "to"           : hdr_to.text.strip()      if hdr_to     is not None else None,
+                    "cc"           : hdr_cc.text.strip()      if hdr_cc     is not None else None,
+                    "message-id"   : hdr_msg_id.text.strip()  if hdr_msg_id is not None else None,
+                    "body"         : msg_body.text.strip()    if msg_body   is not None else None,
+                    "reply_to_url" : in_reply_to,
+                    "attachments"  : attach_list}
+            return item
+        else:
+            print(f"ERROR: {resp.status_code} {msg_url}")
+
+
     def db_prefix(self) -> str:
         return "w3c"
 
@@ -124,14 +194,22 @@ class MailArchiveBackendW3C(MailArchiveBackend):
         self.mailbox  = mailbox
         self.msg_urls = {}
         self.base_url = {}
+
+        tmp_msg_urls = {}
+        tmp_base_url = {}
         count = 0
         list_url = f"https://lists.w3.org/Archives/Public/{self.mailbox}/"
         for index in self._find_indexes(list_url):
             self._log.warning(f"messages_ids {index['url']}")
             for msg_to_fetch in self._find_messages(index["url"]):
-                self.msg_urls[count] = msg_to_fetch['url']
-                self.base_url[count] = index['url']
+                tmp_msg_urls[count] = msg_to_fetch['url']
+                tmp_base_url[count] = index['url']
                 count += 1
+        # Messages on the server are stored in reverse chronological order,
+        # reverse the ids so we retrieve them in date order.
+        for i in range(0, count):
+            self.msg_urls[count - i] = tmp_msg_urls[i]
+            self.base_url[count - i] = tmp_base_url[i]
 
 
     def close_mailbox(self) -> None:
@@ -149,93 +227,42 @@ class MailArchiveBackendW3C(MailArchiveBackend):
 
 
     def fetch(self, message_ids: List[int]) -> Iterator[Tuple[int, bytes]]:
+        msg_id_for_url = {}
+
         for uid in message_ids:
             msg_url  = self.msg_urls[uid]
             base_url = self.base_url[uid]
-    
-            time.sleep(self.delay)
-            resp = self.session.get(msg_url)
-            if resp.status_code == 200:
-                soup = BeautifulSoup(resp.text, 'html.parser')
-                main = soup.find("main", class_="mail")
 
-                headers = main.find("ul", class_="headers")
-                hdr_subject = "Subject: " + soup.find("h1").text.strip()
-                hdr_date    = headers.find("span", class_ = "date")
-                hdr_from    = headers.find("span", class_ = "from")
-                hdr_to      = headers.find("span", class_ = "to")
-                hdr_cc      = headers.find("span", class_ = "cc")
-                hdr_msg_id  = headers.find("span", class_ = "message-id")
+            item = self._fetch_message(msg_url, base_url)
 
-                msg_body = main.find("pre", class_ = "body")
+            print(f"{uid:5} {item['subject']}")
 
-                attach_list = []
-                attachments = main.find("section", class_ = "message-body-part attachment-links")
-                if attachments is not None:
-                    for attach in attachments.find_all("li"):
-                        parts = attach.text.split()
-                        if parts[1] == "attachment:":
-                            if len(parts) == 3 and parts[2] == "stored":
-                                attach_type = parts[0]
-                                attach_disp = "inline"
-                            else:
-                                attach_type = parts[0]
-                                attach_disp = "attach"
-                        else:
-                            print(f"ERROR: can't parse attachment")
-                            print(attach)
-                            sys.exit()
-                        attach_path = attach.find("a")["href"]
-                        attach_url  = base_url + attach_path
-                        attachment = {"media_type"  : attach_type, 
-                                      "url"         : attach_url, 
-                                      "path"        : attach_path,
-                                      "disposition" : attach_disp}
-                        attach_list.append(attachment)
+            if item["message-id"] is not None:
+                msg_id_for_url[msg_url] = item["message-id"]
 
-
-                footer = soup.find("footer")
-                in_reply_to = None
-                for fitem in footer.find_all("li"):
-                    fheader = fitem.find("span", class_="heading")
-                    if fheader is not None:
-                        if fheader.text.strip() == "In reply to" or fheader.text.strip() == "Maybe in reply to":
-                            anchor = fitem.find("a")
-                            if anchor.text == "Message archived in another list or period":
-                                in_reply_to = anchor["href"]
-                            else:
-                                in_reply_to = base_url + anchor["href"]
-
-
-                item = {"msg_url"      : msg_url,
-                        "subject"      : hdr_subject,
-                        "date"         : hdr_date.text.strip()    if hdr_date   is not None else None,
-                        "from"         : hdr_from.text.strip()    if hdr_from   is not None else None,
-                        "to"           : hdr_to.text.strip()      if hdr_to     is not None else None,
-                        "cc"           : hdr_cc.text.strip()      if hdr_cc     is not None else None,
-                        "message-id"   : hdr_msg_id.text.strip()  if hdr_msg_id is not None else None,
-                        "body"         : msg_body.text.strip()    if msg_body   is not None else None, 
-                        "reply_to_url" : in_reply_to,
-                        "attachments"  : attach_list}
-
-                print(f"{uid:5} {item['subject']}")
-
-                # Assemble message from `item`
-                email_msg = EmailMessage()
-                email_msg["From"]       = item["from"][6:]
-                email_msg["To"]         = item["to"][4:]
-                if item["cc"] is not None:
-                    email_msg["Cc"]         = item["cc"][4:]
-                email_msg["Subject"]    = item["subject"][9:]
-                email_msg["Date"]       = item["date"][6:]
-                email_msg["Message-ID"] = item["message-id"][12:]
-                if item["body"] is not None:
-                    email_msg.set_content(item["body"])
+            email_msg = ""
+            if item["from"] is not None:
+                email_msg += f"{item['from']}\r\n"
+            if item["to"] is not None:
+                email_msg += f"{item['to']}\r\n"
+            if item["cc"] is not None:
+                email_msg += f"{item['cc']}\r\n"
+            if item["subject"] is not None:
+                email_msg += f"{item['subject']}\r\n"
+            if item["date"] is not None:
+                email_msg += f"{item['date']}\r\n"
+            if item["message-id"] is not None:
+                email_msg += f"{item['message-id']}\r\n"
+            if item['reply_to_url'] is not None:
+                if item['reply_to_url'] in msg_id_for_url:
+                    email_msg += f"In-Reply-To: {msg_id_for_url[item['reply_to_url']][12:]}\r\n"
                 else:
-                    print(f"No body {uid}")
+                    print("Missing in-reply-to")
+            email_msg += "\r\n"
+            if item["body"] is not None:
+                email_msg += item["body"]
 
-                # FIXME: fetch attachments
-                yield uid, email_msg.as_bytes()
-            else:
-                print(f"ERROR: {resp.status_code} {msg_url}")
+            # FIXME: handle attachments
+
+            yield uid, bytes(email_msg, encoding="utf-8")
 
